5eb017ec13b7b0e0ef7d9506bec96303f0421b40
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2023-09-15 00:53:54 -0400

    Injected error in test case 2 to test the CI response.

--- test_script.ts
+++ test_script.ts
@@ -4,18 +4,18 @@ interface Answers {
     asw_test1: String;
     asw_test2: String;
 }
-let answers: Answers = {
+let expected_answers: Answers = {
     asw_test1: 'Hello World',
-    asw_test2: 'Goodbye World'
+    asw_test2: 'Goodbye1 World'
 };
 
 function test_correct_output1()
 {
     let str: String = test_funcs.output_string1();
 
-    if (str !== answers.asw_test1) {
+    if (str !== expected_answers.asw_test1) {
         console.log("wata mess");
-        throw new TypeError("Test 1 ERROR :: output should have been \"" + answers.asw_test1 + "\" but instead is \"" + str + "\".");
+        throw new TypeError("Test 1 ERROR :: output should have been \"" + expected_answers.asw_test1 + "\" but instead is \"" + str + "\".");
     }
 }
 
@@ -23,8 +23,8 @@ function test_correct_output2()
 {
     let str: String = test_funcs.output_string2();
 
-    if (str !== answers.asw_test2) {
-        throw new TypeError("Test 2 ERROR :: output should have been \"" + answers.asw_test2 + "\" but instead is \"" + str + "\".");
+    if (str !== expected_answers.asw_test2) {
+        throw new TypeError("Test 2 ERROR :: output should have been \"" + expected_answers.asw_test2 + "\" but instead is \"" + str + "\".");
     }
 }
 
